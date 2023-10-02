5fec53dfc927734932d4b2de3b1bd20093fbc8d9
Eleanor Kelman <ekelman3@gmail.com>
2023-10-02 15:17:02 -0400

    completed lecture

--- src/data-structures.py
+++ src/data-structures.py
@@ -1,7 +1,74 @@
 # lists and list comprehension
+    # lists are mutable
+    # lists are 0-indexed and store ordered values
+
+'''
+nums = [1, 2, 3, 4, 5]
+
+# slice: [starting index : ending index]
+
+nums.append(6) # adds new element to end of list
+
+nums.pop(5) # removes (and retrieves) element from given index of list
+
+# forEach loop
+for num in nums:
+    print(num)
+
+nums_squared = []
+
+for num in nums:
+    nums_squared.append(num * num)
+
+nums_squared = [num * num for num in nums] # list comprehension version
+
+nums_cubed = []
+
+for num in nums:
+    if num % 2 == 0:
+        nums_cubed.append(num * num * num)
+
+nums_cubed = [num ** 3 for num in nums if num % 2 == 0] # list comprehension version
+
+print(nums_cubed)
+'''
 
 # dictionaries
+    # dictionaries are mutable
+    # dictionaries store unordered key:value pairs
+
+'''
+squares = {1 : 1, 2 : 4, 3 : 9, 4 : 16, 5 : 25}
+
+print(squares[4])
+
+squares[6] = 36 # adds new key:value pair
+
+print(squares)
+'''
 
 # sets
+    # sets are mutable
+    # sets are unordered and without repeat values
+
+'''
+plants = set()
+
+plants.add("tree")
+plants.add("bush")
+plants.add("flower")
+plants.add("grass")
+plants.add("bush") # will not be added (repeat value)
+
+print(plants)
+'''
 
 # tuples
+    # tuples are immutable
+    # tuples are 0-indexed and store related values
+
+'''
+color = (9, 18, 200)
+
+print(color)
+'''
--- src/fundamentals.py
+++ src/fundamentals.py
@@ -2,10 +2,109 @@
     # string concatenation: "string1" + "string2"
     # f-string interpolation: f"{string1}{string2}"
 
+    # typecasting: changing one data type to another
+
+'''
+first_name = "Eleanor"
+last_name = "Kelman"
+
+favorite_number = -7
+print(first_name + "'s favorite number is " + str(favorite_number))
+print(f"{first_name}'s favorite number is {favorite_number}")
+'''
+
 # conditional statements
+'''
+if first_name == "Eleanor":
+    print("Hello there, Eleanor!")
+
+if first_name == "Tyler" or last_name == "Taylor":
+    print("Hello there, Tyler!")
+else:
+    print("You're not Tyler!")
+
+if favorite_number < 0:
+    print("The number is negative!")
+elif favorite_number == 0:
+    print("The number is exactly 0!")
+else:
+    print("The number is positive!")
+
+if favorite_number < 0:
+    print("The number is negative!")
+if favorite_number == 0:
+    print("The number is exactly 0!")
+if favorite_number > 0:
+    print("The number is positive!")
+
+# ternary operator
+print("The number is a big number!") if favorite_number > 5 else print("The number is a small number!")
+'''
 
 # loops and sequences
-    # for loops
-    # while loops
+    # for loops: run for duration of a sequence
+    # while loops: run for so long as a condition is met
+
+'''
+for i in range(10):
+    print(i)
+
+print("")
+
+for i in range(5, 10):
+    print(i)
+
+print("")
+
+for i in range(5, 10, 2):
+    print(i)
+
+for i in range(7, 0, -1):
+    print(i)
+
+counter = 0
+
+while counter < 10:
+    print(counter)
+    counter = counter + 1 # counter += 1
+
+secret_word = ""
+
+while not secret_word == "you guessed it":
+    secret_word = input("Guess the secret word (it's actually a sentence muahahaa)")
+
+while_loop_counter = 0
+
+# WEEOOWEEOO DANGER INFINITE LOOP ALERT!
+while True:
+    print("lmao")
+    if while_loop_counter > 6:
+        break # exits a loop prematurely
+    while_loop_counter += 1
+
+'''
 
 # functions
+
+'''
+# function definition
+def prints_hello_world():
+    print("hello world")
+
+# function call
+prints_hello_world()
+
+def prints_hello_to_you(name):
+    print("Hello, " + name)
+
+prints_hello_to_you("Mario")
+prints_hello_to_you("Luigi")
+
+def adds_two_numbers(num1, num2):
+    sum = num1 + num2
+    return sum
+    return num1 + num2
+
+sum = adds_two_numbers(3, 4)
+print(sum)
+'''
